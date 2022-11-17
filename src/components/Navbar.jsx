97de892d0035a3d10951cccfd7b38cf61451a731
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 02:29:53 -0300

    feat: :fire: adicionando navbar e refatorando

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,31 @@ import logo from '../images/logo.png';
 import { Link } from 'react-scroll'
 
 function Navbar() {
+
     const [nav, setNav] = useState(false);
+
     const changeBackground = () => {
         if (window.scrollY >= 50) {
             setNav(true)
         }
-        else{
+        else {
             setNav(false);
         }
     }
     window.addEventListener('scroll', changeBackground)
+
     return (
-        <nav className={nav ? "nav active" :"nav"}>
+        <nav className={nav ? "nav active" : "nav"}>
             <Link to='#' className='logo'>
-                <img src={logo} alt="" />
+                <img src={logo} alt='' />
             </Link>
+            <input type="checkbox" className='menu-btn' id='menu-btn' />
+            <label for="menu-btn" className='menu-icon'>
+                <span className='nav-icon'></span>
+            </label>
+            <ul className='menu'>
+                <li><Link to='#'></Link></li>
+            </ul>
         </nav>
     )
 }
