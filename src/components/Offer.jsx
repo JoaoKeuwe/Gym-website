c91ab957d283bf816aea9bc0f98ad5983349d86d
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 04:29:14 -0300

    feat: :fire: Adicionando e alterando o componente de oferta

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -4,7 +4,7 @@ function Offer() {
   return (
     <div id='presentaion'>
       <div className='pr-heading'>
-        <h1>A MELHOR <span>OFERTA</span> PARA O VERÃO </h1>
+        <h1>A MELHOR <span>OFERTA</span> PARA O VERÃO <span>!!</span> </h1>
         <p className='details'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <div className='pr-btns'>
           <a href="#" className='pr-btn'> FAÇA PARTE</a>
