1960e57fdbb9ca6e0b6e639914da9bc0c96b4a16
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 03:58:56 -0300

    feat: :zap: criando componente de oferta

---
+++ src/components/Offer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Offer() {
+  return (
+    <div id='presentaion'>
+      <div className='pr-heading'>
+        <h1>A MELHOR <span>OFERTA</span> PARA O VERÃO </h1>
+        <p className='details'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className='pr-btns'>
+          <a href="#" className='pr-btn'> FAÇA PARTE</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Offer
