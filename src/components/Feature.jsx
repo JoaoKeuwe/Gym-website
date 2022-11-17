c92ac73c860872105327773289e17229551ac32c
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 03:38:44 -0300

    feat: :zap: Adicionando componente de serviços

---
+++ src/components/Feature.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import FeatureBox from './Featurebox'
+import fimage1 from '../images/1.svg'
+import fimage2 from '../images/2.svg'
+import fimage3 from '../images/3.svg'
+import fimage4 from '../images/4.svg'
+
+
+function Feature() {
+  return (
+    <div id='features'>
+      <h1>
+       SERVIÇOS
+        <div className='a-container'>
+        <FeatureBox image={fimage1} title='Levantamento de peso'/>
+        <FeatureBox image={fimage2} title='Aeróbico'/>
+        <FeatureBox image={fimage3} title='Hipertrofia'/>
+        <FeatureBox image={fimage4} title='Aulas Práticas'/>
+        </div>
+      </h1>
+    </div     >
+  )
+}
+
+export default Feature
