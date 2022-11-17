a5d4420fcfab7020e57f31bb0ee4bd6c7d8bf1bf
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 03:38:57 -0300

    feat: :zap: renderizando componente de serviços

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import Feature from './components/Feature'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <Navbar />
      <Header />
+     <Feature />
     </div>
   );
 }
