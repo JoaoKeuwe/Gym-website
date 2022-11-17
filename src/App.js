0a0f464831233fe4ca3e992cb50f632215e9a1be
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 05:01:59 -0300

    refactor: :bug: Refatorando codigo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import Feature from './components/Feature'
-import Offer from './components/Offer'
-import About from './components/About'
-import Contact from './components/Contact'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Feature from "./components/Feature";
+import Offer from "./components/Offer";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Header />
-     <Feature />
-     <Offer />
-     <About />
-     <Contact />
+      <Navbar />
+      <Header />
+      <Feature />
+      <Offer />
+      <About />
+      <Contact />
     </div>
   );
 }
