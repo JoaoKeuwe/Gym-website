6bb310c83b6d61b4acaa8faaeb9ce5e22979ad59
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 04:23:13 -0300

    feat: :zap: componente about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import aboutimage from '../images/about.png'
 
 function About() {
   return (
-    <div>
-      About
+    <div id='about'>
+      <div className='about-image'>
+        <img src={aboutimage} alt="" />
+      </div>
+      <div className='about-text'>
+        <h1>SAIBA MAIS SOBRE NÓS</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem a minus magni eum dolor voluptatem cupiditate sapiente temporibus. Suscipit autem ullam officia consectetur, saepe rerum repudiandae esse dicta tempore alias.</p>
+        <button>LEIA MAIS</button>
+      </div>
     </div>
   )
 }
