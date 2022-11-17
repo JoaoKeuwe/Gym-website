2dd2e3c4c3ffa23d2e3b09d9613b7dca0ca67c96
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 04:23:01 -0300

    feat: :zap: fazendo formulario que será na parte de contatos

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div id='contact'>
+      <h1>FALE <span className='span1'>CONOSCO</span> </h1>
+      <form>
+          <input type="text" placeholder='Nome completo' required />
+          <input type="email" placeholder='Digite seu E-mail' required />
+          <textarea name="message" placeholder='Escreva aqui...'></textarea>
+          <input type="submit"value='Enviar' />
+      </form>
+
+    </div>
+  )
+}
+
+export default Contact
