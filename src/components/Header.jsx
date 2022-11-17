36b19e49f22403dd917e2fba176fde5e8d2d7998
joaokeuwe <joaov2872@hotmail.com>
2022-11-17 03:06:08 -0300

    feat: :zap: adicionando Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div id='main'>
+      <div className='name'>
+        <h2>INTENSIFIQUE SEUS</h2>
+        <h1><span>TREINOS</span> COM A GENTE</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
